11ee24bc45d6ab18feb13f59102b27d33ae416ec
Basel85 <baselbasel1515@gmail.com>
2024-03-03 17:36:56 +0200

    some refactoring

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:posts_app_flutter/core/injection_container/injection_container.dart';
 import 'package:posts_app_flutter/features/posts/presentation/bloc/add_post/add_post_bloc.dart';
-import 'package:posts_app_flutter/features/posts/presentation/bloc/posts/posts_bloc.dart';
 import 'package:posts_app_flutter/features/posts/presentation/screens/add_post_screen.dart';
 import 'package:posts_app_flutter/features/posts/presentation/screens/posts_screen.dart';
 
@@ -10,16 +9,11 @@ class AppRouter {
   static Route onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => MultiBlocProvider(
-          providers: [
-            BlocProvider(create: (context) => getIt<PostsBloc>()),
-          ],
-          child: const PostsScreen()));
+        return MaterialPageRoute(builder: (_) => const PostsScreen());
       case '/add-post':
         return MaterialPageRoute(builder: (_) => MultiBlocProvider(
           providers: [
             BlocProvider(create: (context) => getIt<AddPostBloc>()),
-            BlocProvider(create: (context) => getIt<PostsBloc>()),
           ],
           child: const AddPostScreen()));
       default:
--- lib/posts_app.dart
+++ lib/posts_app.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:posts_app_flutter/core/helpers/app_route_paths.dart';
+import 'package:posts_app_flutter/core/injection_container/injection_container.dart';
 import 'package:posts_app_flutter/core/router/app_router.dart';
+import 'package:posts_app_flutter/features/posts/presentation/bloc/posts/posts_bloc.dart';
 
 class PostsApp extends StatelessWidget {
   const PostsApp({super.key});
@@ -12,15 +15,18 @@ class PostsApp extends StatelessWidget {
       
       designSize: const Size(360, 690),
       minTextAdapt: true,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Posts App Using Clean Architecture',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+      child: BlocProvider(
+        create: (context) => getIt<PostsBloc>(),      
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Posts App Using Clean Architecture',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          onGenerateRoute: AppRouter.onGenerateRoute,
+          initialRoute: AppRoutePaths.home,
         ),
-        onGenerateRoute: AppRouter.onGenerateRoute,
-        initialRoute: AppRoutePaths.home,
       ),
     );
   }
